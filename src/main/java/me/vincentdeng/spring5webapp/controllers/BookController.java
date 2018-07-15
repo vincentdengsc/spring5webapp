d6d702b7386ad19306980869d6681ed005e4a942
vincent <admin@vincentdeng.net>
2018-07-15 20:13:55 +1000

    Add Books View

---
+++ src/main/java/me/vincentdeng/spring5webapp/controllers/BookController.java
@@ -0,0 +1,25 @@
+package me.vincentdeng.spring5webapp.controllers;
+
+import me.vincentdeng.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Vincent Deng on 15/7/18
+ */
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
