c2d3b252efb73e627c6d484ac0ef94c5f3980b84
vincent <admin@vincentdeng.net>
2018-07-15 20:19:38 +1000

    Add Authors Controller and View

---
+++ src/main/java/me/vincentdeng/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,25 @@
+package me.vincentdeng.spring5webapp.controllers;
+
+import me.vincentdeng.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Vincent Deng on 15/7/18
+ */
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
